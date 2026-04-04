b39025157342c331c069e34920023069291f20d1
Halicade <qstr23@hotmail.com>
2026-04-04 07:51:50 -0400

    initial upload

---
+++ Source/InternalDefs.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MassProductionExpansion;
+
+[DefOf]
+public class InternalDefs
+{
+    public static ThingDef FabricationBench;
+
+    public static ThingDef DrugLab;
+
+    public static ThingDef VFEFactory_AutomatedAssembler;
+
+    public static ThingDef VFEFactory_MedicineGranulator;
+
+    public static ProcessorTemplateDef VFEFactory_Assembler_;
+
+    public static ProcessorTemplateDef VFEFactory_Medicine_;
+
+    //Required so the game doesn't yell at you if not loaded
+    [MayRequire("oskarpotocki.vanillavehiclesexpanded")]
+    public static ThingDef VVE_GarageBench;
+
+    [MayRequire("FrozenSnowFox.FrozenSnowFoxTweaks")]
+    public static ThingDef FSFBionicsTable;
+
+    static InternalDefs() {
+        DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefs));
+    }
+}
---
+++ Source/ProcessorTemplateDef.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace MassProductionExpansion;
+
+public class ProcessorTemplateDef : Def
+{
+    public bool spawnOnInteractionCell;
+    public bool autoGrabFromHoppers;
+    public List<IntVec3> autoInputSlots;
+    public bool isFactoryProcess;
+    public bool autoExtract;
+    public bool onlyGrabAndOutputToFactoryHoppers;
+    public bool disallowMixing;
+    public bool sustainerWhenWorking;
+    public SoundDef sustainerDef;
+    public bool effecterWhenWorking;
+    public EffecterDef effecterDef;
+    public float maxOutputCount;
+}
